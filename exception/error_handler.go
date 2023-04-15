dd1aecb04b25ac5c28f45958710d43273007dd20
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-04-15 12:22:27 +0700

    fix: error handler

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -4,6 +4,7 @@ import (
 	"be-ifid/internal/response"
 	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -16,9 +17,16 @@ func ErrorHandler(ctx *fiber.Ctx, err error) error {
 		code = e.Code
 	}
 
+	var data interface{}
+	if strings.EqualFold(err.Error(), http.StatusText(code)) {
+		data = nil
+	} else {
+		data = err.Error()
+	}
+
 	return ctx.JSON(response.BaseResponse{
 		Code:    code,
 		Message: http.StatusText(code),
-		Data:    err.Error(),
+		Data:    data,
 	})
 }
