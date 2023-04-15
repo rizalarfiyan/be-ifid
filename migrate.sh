7b873ab3e9a7c3395001032a70bcd37c99ea172e
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-04-15 10:21:33 +0700

    feat: update read .env in migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -8,12 +8,15 @@ if ! [ -x "$(command -v sql-migrate)" ]; then
 fi
 
 # Load .env
-unamestr=$(uname)
-if [ "$unamestr" = 'Linux' ]; then
-    export $(grep -v '^#' .env | xargs -d '\n')
-elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
-    export $(grep -v '^#' .env | xargs -0)
-fi
+ENV_VARS="$(cat .env | awk '!/^\s*#/' | awk '!/^\s*$/')"
+
+eval "$(
+    printf '%s\n' "$ENV_VARS" | while IFS='' read -r line; do
+        key=$(printf '%s\n' "$line"| sed 's/"/\\"/g' | cut -d '=' -f 1)
+        value=$(printf '%s\n' "$line" | cut -d '=' -f 2- | sed 's/"/\\\"/g')
+        printf '%s\n' "export $key=\"$value\""
+    done
+)"
 
 # Do Action
 OPTIONS="-config=dbconfig.yml -env=be_ifid"
